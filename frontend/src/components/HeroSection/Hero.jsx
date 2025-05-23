6ea00c7b13897e1893b9bf818fd7b715bffbbbb8
desktop-ck1htmj omkar <desktop-ck1htmj.omkar@default.com>
2025-05-23 19:38:58 +0530

    make leading responsive

--- frontend/src/components/HeroSection/Hero.jsx
+++ frontend/src/components/HeroSection/Hero.jsx
@@ -1,20 +1,21 @@
 import myPic from "../../assets/images/omkar.jpg";
+
 const HeroSection = () => {
   return (
-    <section className=" h-screen flex items-center justify-center bg-gradient-to-br from-amber-200 to-green-200">
-      <div className=" p-4 flex items-center gap-10 justify-center flex-col  flex-wrap sm:flex-row " >
-        <div className="flex justify-center items-center w-[280px] h-[280px] rounded-[50%] overflow-hidden">
-          <img className="w-[100%]" src={myPic} alt="mypic" />
+    <section className="h-screen flex items-center justify-center bg-gradient-to-br from-amber-200 to-green-200">
+      <div className="p-4 flex flex-col sm:flex-row flex-wrap items-center justify-center gap-10">
+        <div className="w-[280px] h-[280px] rounded-full overflow-hidden flex items-center justify-center">
+          <img className="w-full object-cover" src={myPic} alt="Omkar" />
         </div>
-        <div className="flex flex-col font-bold leading-10 text-3xl p-8 lg:max-w-[55vw] md:leading-15 md:text-5xl ">
-          <h1 className="text-2xl backdrop-blur-2xs bg-[#ffffff3f] font-medium w-fit rounded-2xl px-5 py-2 ">
+        <div className="flex flex-col p-8 font-bold text-3xl leading-10 sm:text-4xl md:text-5xl md:leading-[3.8rem] lg:max-w-[55vw]">
+          <h1 className="text-2xl sm:text-3xl font-medium w-fit bg-[#ffffff3f] backdrop-blur-sm rounded-2xl px-5 py-2 mb-4">
             Hi, I’m Omkar 👋
           </h1>
           <p>I build responsive, clean, and modern websites — with purpose and precision.</p>
         </div>
       </div>
     </section>
-  )
+  );
 };
 
 
