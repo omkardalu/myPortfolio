c85ed15de82530e619ac65e325bc1784ab99411a
desktop-ck1htmj omkar <desktop-ck1htmj.omkar@default.com>
2025-05-24 13:58:35 +0530

    add images and project section

--- frontend/src/components/ProjectsSection/ProjectCard.jsx
+++ frontend/src/components/ProjectsSection/ProjectCard.jsx
@@ -1,22 +1,23 @@
 import { Link } from "react-router-dom";
 import githubLogo from "../../assets/images/github-logo.png";
 import upRightArrowLogo from "../../assets/images/up-right-arrow.png";
-const ProjectCard = ({ name, img, description, link, github }) => {
+const ProjectCard = ({ name, img, description, tools, link, github }) => {
   return (
-    <section className="flex w-[350px] flex-col justify-center rounded-2xl p-4 bg-[#ffffff3c] backdrop-blur-xs">
+    <section className="flex w-[350px] flex-col shadow-2xl m-4 justify-center rounded-2xl bg-[#ffffff3c] p-4 backdrop-blur-xs">
       <div className="text-2xl capitalize">{name}</div>
-      <div className="mt-3 flex h-[180px] shadow-2xl w-[100%] items-center justify-center overflow-hidden rounded-2xl">
+      <div className="mt-3 flex h-[180px] w-[100%] items-center justify-center overflow-hidden rounded-2xl shadow-2xl">
         <img className="h-full" src={img} alt={name} />
       </div>
-      <div className="h-[100px] mt-3 p-2 text-xl text-[0.9rem] font-[500] text-shadow-neutral-900">
+      <div className="mt-3 h-[100px] p-2 text-xl text-[0.9rem] font-[500] text-shadow-neutral-900">
         {description}
       </div>
-      <div className="flex w-full items-center justify-between p-4">
+      <p className="font-mono text-[0.9rem] ml-2">Built on: {tools}</p>
+      <div className="flex w-full items-center justify-between p-3">
         <Link to={github} target="_blank">
           <img className="mr-3 w-6" src={githubLogo} alt="github" />
         </Link>
         <Link to={link} target="_blank">
-          <img className="mr-3 w-6" src={upRightArrowLogo} alt="arrow" />
+          <img className="mr-3 w-4" src={upRightArrowLogo} alt="arrow" />
         </Link>
       </div>
     </section>
--- frontend/src/components/ProjectsSection/Projects.jsx
+++ frontend/src/components/ProjectsSection/Projects.jsx
@@ -1,39 +1,78 @@
-import ProjectCard from './ProjectCard.jsx';
-import Project from './ProjectCard.jsx';
+import ProjectCard from "./ProjectCard.jsx";
 const Projects = () => {
+  const projects = [
+    {
+      name: "write&X",
+      img: "./public/write-x.png",
+      description:
+        "A Blog posting website where users can login, create blogs,like,share, follow, search etc. with professional text formater  ",
+      tools: "CSS, React, Express, sqlite3",
+      link: "https://writeandx.onrender.com",
+      github: "https://github.com/omkardalu/shivaji-omkar-write-X/tree/main",
+    },
+    {
+      name: "Quiz time",
+      img: "./public/quiz-time.png",
+      description:
+        "An interactive quiz application that allows users to test their knowledge, with features like timer, music and Score Board.",
+      tools: "HTML, CSS, JavaScript",
+      link: "https://omkardalu.github.io/quiz-time/",
+      github: "https://github.com/omkardalu/quiz-time",
+    },
+    {
+      name: "window clone",
+      img: "./public/windows-clone.png",
+      description:
+        "A windows desktop clone with File Explorer, start menu and taskbar with HTML and CSS",
+      tools: "HTML, CSS",
+      link: "https://medium-clone-omkar.netlify.app/",
+      github: "https:github.com/omkardalu",
+    },
+    {
+      name: "apple clone",
+      img: "./public/apple-clone.png",
+      description:
+        "A clone of the Apple website's homepage UI, showcasing its products and features.",
+      tools: "HTML, CSS",
+      link: "https://omkardalu.github.io/apple-clone/",
+      github: "https://github.com/omkardalu/apple-clone",
+    },
+    {
+      name: "squareSpace clone",
+      img: "./public/squarespace-clone.png",
+      description:
+        "A clone of the squarespace website homepage UI, showcasing its products and features.",
+      tools: "HTML, CSS",
+      link: "https://omkardalu.github.io/Squarespace/",
+      github: "https://github.com/omkardalu/Squarespace",
+    },
+    {
+      name: "flipkart clone",
+      img: "./public/flipkart-clone.png",
+      description:
+        "A clone of the Flipkart website homepage UI, showcasing its products and features.",
+      tools: "HTML, CSS",
+      link: "https://jovial-puppy-c62964.netlify.app/",
+      github: "https://github.com/path-tw/my-clone-omkardalu",
+    },
+  ];
+  const projectsList = projects.map((project) => (
+    <ProjectCard
+      key={project.name}
+      name={project.name}
+      img={project.img}
+      description={project.description}
+      tools={project.tools}
+      link={project.link}
+      github={project.github}
+    />
+  ));
   return (
-    <section id="projects" className='mt-10'>
-      <div className='flex flex-wrap gap-4 grow items-center justify-evenly'>
-        <ProjectCard 
-        name={'medium'}
-        img={'https://i.imgur.com/2b6tKqk.png'}
-        description={'A blogging platform where users can write and share their articles.'}
-        link={'https://medium-clone-omkar.netlify.app/'}
-        github={'https:github.com/omkardalu'}
-        />
-        <ProjectCard 
-        name={'medium'}
-        img={'https://i.imgur.com/2b6tKqk.png'}
-        description={'A blogging platform where users can write and share their articles.'}
-        link={'https://medium-clone-omkar.netlify.app/'}
-        github={'https:github.com/omkardalu'}
-        />
-        <ProjectCard 
-        name={'medium'}
-        img={'https://i.imgur.com/2b6tKqk.png'}
-        description={'A blogging platform where users can write and share their articles.'}
-        link={'https://medium-clone-omkar.netlify.app/'}
-        github={'https:github.com/omkardalu'}
-        />
-        <ProjectCard 
-        name={'medium'}
-        img={'https://i.imgur.com/2b6tKqk.png'}
-        description={'A blogging platform where users can write and share their articles.'}
-        link={'https://medium-clone-omkar.netlify.app/'}
-        github={'https:github.com/omkardalu'}
-        />
+    <section id="projects" className="mt-10">
+      <div className="flex grow flex-wrap items-center justify-center">
+        {projectsList}
       </div>
     </section>
-  )
+  );
 };
 export default Projects;
