b864ae935f9053e204f75d547f3389ea58a9253d
desktop-ck1htmj omkar <desktop-ck1htmj.omkar@default.com>
2025-05-26 13:22:08 +0530

    make the blog responsive to mobile

--- frontend/public/docs/readme.md
+++ frontend/public/docs/readme.md
@@ -12,18 +12,24 @@ This is still in process. I don't understand how to do this markdown fully worka
 
 # What i did -
 - I installed the [react-markdown](https://www.npmjs.com/package/react-markdown) 
-# *Setup* 
- -npm i react-markdown
+- *Setup* 
+- npm i react-markdown
+
 ```
- import content from 'file';
- import ReactMarkdown from 'react-markdown'
- import remarkGfm from 'remark-gfm'
- const blog = () => {
-  return (
-     <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
-  )
- }
- ```
+import content from 'file';
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+const blog = () => {
+ return (
+    <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+ )
+}
+```
+
+| 1 | 2 |
+|---|---|
+| 3 | 4 |
+| 5 | 6 |
 
 but still its not working as expected
-# *for source [**git**](https://github.com/omkardalu/myPortfolio/blob/main/frontend/src/pages/Blog.jsx)*
+## *for source [**git**](https://github.com/omkardalu/myPortfolio/blob/main/frontend/src/pages/Blog.jsx)*
--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -10,11 +10,29 @@ const Blog = () => {
       .then((data) => setContent(data));
   }, []);
   return (
-    <article className="mx-auto max-w-3xl p-6 flex justify-center" >
-      <div  className="prose flex-col justify-center rounded-2xl border-2 border-transparent bg-[#ffffff3c] p-3 text-gray-700 shadow-2xl">
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+    <section className="py-8 px-4 flex items-center justify-center">
+      <div className="prose break-words flex-col justify-center max-w-full rounded-2xl border-2 border-transparent bg-[#ffffff3c] p-8 shadow-2xl">
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]}
+          components={{
+            h1: ({node, ...props}) =><> <h1 className="text-4xl font-semibold mt-4   mb-4" {...props} /><hr/></>,
+            h2: ({node, ...props}) => <h2 className="text-3xl font-semibold mt-3 mb-3" {...props} />,
+            h3: ({node, ...props}) => <h3 className="text-2xl font-semibold mt-2 mb-2" {...props} />,
+            ul: ({node, ...props}) => <ul className="list-disc list-inside ml-4 mb-4" {...props} />,
+            ol: ({node, ...props}) => <ol className="list-decimal list-inside ml-6 mb-4" {...props} />,
+            li: ({node, ...props}) => <li className="mb-1" {...props} />,
+            pre: ({node, ...props}) => <pre className="bg-white/10 rounded-xl  p-4 overflow-auto backdrop-blur-2xl my-2" {...props} />,
+            p: ({node, ...props}) => <p className="pb-1.5 pt-1.5 " {...props} />,
+            code: ({node, ...props}) => <code className="bg-white/20 rounded-sm p-0.5 backdrop-blur-2xl" {...props} />,
+            a: ({node, ...props}) => <a className="underline " {...props} />,
+            tr: ({node, ...props}) => <tr className=" mt-2 border-b-1 w-fit " {...props} />,
+            td: ({node, ...props}) => <td className=" p-2" {...props} />,
+          }}
+        >
+          {content}
+        </ReactMarkdown>
       </div>
-    </article>
+    </section>
   );
 };
 
