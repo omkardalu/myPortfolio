e964c21db772ba84195689be5711ddd581ea8742
desktop-ck1htmj omkar <desktop-ck1htmj.omkar@default.com>
2025-10-28 21:29:28 +0530

    make the skill section responsive

--- frontend/src/components/skillSection/Skills.jsx
+++ frontend/src/components/skillSection/Skills.jsx
@@ -1,30 +1,30 @@
 import SkillCard from "./SkillCard";
 
 const Skills = () => {
-    const skills = [
+  const skills = [
     {
-      name:"HTML",
-      image:"./images/skillLogos/html-logo.svg",
+      name: "HTML",
+      image: "./images/skillLogos/html-logo.svg",
     },
     {
-      name:"CSS",
-      image:"./images/skillLogos/css-logo.svg",
+      name: "CSS",
+      image: "./images/skillLogos/css-logo.svg",
     },
     {
-      name:"JavaScript",
-      image:"./images/skillLogos/JavaScript-logo.png",
+      name: "JavaScript",
+      image: "./images/skillLogos/JavaScript-logo.png",
     },
     {
-      name:"React.js",
-      image:"./images/skillLogos/react-logo.svg",
+      name: "React.js",
+      image: "./images/skillLogos/react-logo.svg",
     },
     {
-      name:"tailwind",
-      image:"./images/skillLogos/tailwind-logo.svg",
+      name: "tailwind",
+      image: "./images/skillLogos/tailwind-logo.svg",
     },
     {
-      name:"git/github",
-      image:"./images/skillLogos/git-logo.svg",
+      name: "git/github",
+      image: "./images/skillLogos/git-logo.svg",
     },
     // {
     //   name:"CPP",
@@ -40,11 +40,12 @@ const Skills = () => {
     // },
   ];
   return (
-    <div className="flex flex-wrap flex-col items-center justify-center rounded-lg  border-2 border-transparent bg-[var(--card-bg-color)] p-8 py-4 shadow-2xl max-sm:mx-12">
+    <div className="flex flex-col flex-wrap items-center gap-2  justify-center rounded-lg border-2 border-transparent bg-[var(--card-bg-color)] m-4 p-4 shadow-2xl">
       <span className="self-start text-3xl font-bold">Skills</span>
-      <div className="flex flex-wrap  items-center justify-center">
-      {skills.map((skill)=><SkillCard skillName={skill.name} image={skill.image} />)}
-
+      <div className="flex flex-wrap bg-[#ffffff1d] rounded-lg columns-3 items-center justify-center">
+        {skills.map((skill) => (
+          <SkillCard skillName={skill.name} image={skill.image} />
+        ))}
       </div>
     </div>
   );
